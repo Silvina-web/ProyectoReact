96c94418975404aff26330354d3e637eb7152cd4
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-11-17 11:06:21 -0300

    No carga farebase

--- src/assets/firebase.js
+++ src/assets/firebase.js
@@ -1,15 +1,12 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-import {getFirestore, collection, addDoc} from "firebase/firestore"
+import { getFirestore, collection, addDoc} from "firebase/firestore" 
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  
-  apiKey: process.env.API_KEY,
+  /*  apiKey: "AIzaSyDQUEH2IEYp0idmUlXJFmPSptyP8weYYNY",  */
+  apiKey: process.env.API_KEY,  
   authDomain: "lisa-arteenceramica.firebaseapp.com",
   projectId: "lisa-arteenceramica",
   storageBucket: "lisa-arteenceramica.appspot.com",
@@ -17,24 +14,26 @@ const firebaseConfig = {
   appId: "1:393415337359:web:34e50071619074245417fa"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 
 const db = getFirestore()
 
-const cargarBDD = async ()=>{
+const cargarBDD = async ()=> {
   const promise = await fetch ('./json/productos.json')
   const productos = await promise.json()
-  productos.forEach(async (prod)=>{
-    await addDoc(collection(db, "productos"), {
+  productos.forEach(async (prod)=> {
+
+    await addDoc(collection (db, "productos"), {
       nombre: prod.nombre, 
       dimensiones: prod.dimensiones,
       precio: prod.precio,
       stock: prod.stock, 
       idCategoria: prod.idCategoria, 
       img: prod.img
-    })
+    }
+    )
 
 
   })
@@ -42,5 +41,10 @@ const cargarBDD = async ()=>{
 
 }
 
-export {cargarBDD}
 
+
+
+
+export {cargarBDD}
+ 
+ 
--- src/assets/funciones.js
+++ src/assets/funciones.js
@@ -1,12 +1,14 @@
-import {cargarBDD} from "./firebase"
+import {cargarBDD} from "./firebase" 
 
 
 export const consultarJson = async (ruta)=>{
     const promise = await fetch (ruta)
     const productos = await promise.json ()
     return productos
-  
-}
 
-cargarBDD()
+    
+  
+} 
+
+ cargarBDD() 
 
